9d2b18a2e14480cc340a11fe09399fe95a843ee9
Manuel Rúa <manuruaco@gmail.com>
2021-10-25 18:59:05 +0200

    Product context

---
+++ internal/products/domain/product_test.go
@@ -0,0 +1,19 @@
+package products
+
+import "testing"
+
+func TestNewProduct(t *testing.T) {
+	sku := "KASL-3423"
+	expected := Product{
+		sku: sku,
+	}
+
+	product := NewProduct(sku)
+	if product != expected {
+		t.Errorf("Expected: %v, got: %v", expected, product)
+	}
+
+	if product.SKU() != sku {
+		t.Errorf("Expected: %s, got: %s", product.SKU(), sku)
+	}
+}
